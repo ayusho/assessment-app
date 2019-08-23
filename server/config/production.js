3fa82205ad6f9a7647dbf9e5b825d75eeed2f696
OJHA Ayush <ayush.ojha@soprasteria.com>
2019-08-23 23:22:34 +0530

    production db url update

--- server/config/production.js
+++ server/config/production.js
@@ -1,6 +1,6 @@
 module.exports = {
     logging: false,
     db: {
-        url: 'mongodb://admin:Password@123@ds155916.mlab.com:55916/assessmentapp'
+        url: 'mongodb://admin:Password123@ds155916.mlab.com:55916/assessmentapp'
       }
 }
